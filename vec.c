ae4c5ce5b9553e571c1902728f1388aaef107872
lloydkirk <kirklloyd@gmail.com>
2021-09-08 20:30:15 +0000

    updates to vector implementation

--- vec.c
+++ vec.c
@@ -2,109 +2,110 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_ARRAY_SIZE 10000000 // 10,000,000
 #define MAX_ARRAY_DIM 256       // maximum allowed dimensions
 
-// #define VC_ARRAY_TYPE(type) vc_vec_##type##_t
-//
-// #define __VC_ARRAY_TYPEDEF(type) \
-//   typedef struct vc_vec_##type##_s { \
-//     size_t len; \
-//     size_t itemsize; \
-//     size_t dims[MAX_ARRAY_DIM]; \
-//     size_t ndims; \
-//     type **data; \
-//   } VC_ARRAY_TYPE(type)
-//
-// __VC_ARRAY_TYPEDEF(uint64_t);
-// __VC_ARRAY_TYPEDEF(float);
-// __VC_ARRAY_TYPEDEF(double);
-
-// #define VC_ARRAY_ITER_INDICES(arr, type) {
-
-// typedef union {
-//   vc_vec_uint64_t_t u64;
-//   vc_vec_float_t flt;
-//   vc_vec_double_t dbl;
-// } vc_array_t;
-
-// #define __VC_ARRAY_ITER_INDICES(type) { \
-//     if(strcmp(type, "u64")==0) {
-//         arr = arr.u64
-//     }
-// uint64_t iter_indices(vc_array_t arr) {
-//   arr.u64
-//   return 0;
-// }
-// }
-
-typedef enum dtype { UINT64, FLOAT, DOUBLE } vc_dtype_t;
-
-typedef struct vc_vec_s {
-  size_t len;
-  size_t itemsize;
-  size_t dims[MAX_ARRAY_DIM];
-  size_t ndims;
-  vc_dtype_t dtype;
-  union {
-    uint64_t *u64;
-    float *flt;
-    double *dbl;
-    // complex??
-  } data;
-} vc_vec_t;
-
-vc_vec_t *new_vc_vec(size_t ndims) { vc_vec_t arr = {.ndims = ndims}; }
-
-uint64_t iter_indices(vc_array_t arr) { return 0; }
-
-uint64_t prod(uint64_t values, ...) {
-  // uint64_t result = nums[0];
-  // for (int i = 1; i < 10; i++) {
-  //  result *= nums[i];
-  //}
-  va_list args;
-  va_start(args, values);
-  uint64_t result = va_arg(args, uint64_t);
-  printf("%lu\n", values);
-  for (uint64_t i = 1; i < values; ++i) {
-    printf("%lu\n", i);
-    result *= va_arg(args, uint64_t);
+#define VC_ARRAY_TYPE(type) vc_vec_##type##_t
+const char *__vc_type_to_format_str(char *type) {
+  if (strcmp(type, "float") == 0) {
+    return " %f";
+  } else if (strcmp(type, "double") == 0) {
+    return " %f";
+  } else if (strcmp(type, "uint64_t") == 0) {
+    return " %lu";
   }
-  va_end(args);
-  return result;
+  return "xxxfail"; // TODO: fail
 }
 
-  uint64_t iter_indices(vc_array_t arr) { arr.u64 return 0; }
+#define __VC_ARRAY_TYPEDEF(type)                                               \
+  typedef struct vc_vec_##type##_t {                                           \
+    size_t len;                                                                \
+    size_t itemsize;                                                           \
+    size_t ndims;                                                              \
+    size_t dims[MAX_ARRAY_DIM];                                                \
+    type **data;                                                               \
+  } VC_ARRAY_TYPE(type);
 
-  // #define VC_NEW_ARRAY(type, ...) \
-//     __vc_array_typedef(type, ) \
-
-  // #define call_vla(...)                                                          \
-//   do {                                                                         \
-//     int args[] = {__VA_ARGS__};                                                \
-//     for (size_t i = 0; i < sizeof(args) / sizeof(*args); i++)                  \
-//       printf("%zu\t%i\n", i, args[i]);                                         \
-//   } while (0)
-
-  // #define __ARRAY_INIT(
-
-  int main() {
-    // __vc_array_typedef(uint32_t, 2 * 3, 2, 3);
-    // vc_vecuint32_t_t *a = &(vc_vecuint32_t_t){.len = 2 * 3,
-    //                                           .itemsize = sizeof(uint32_t),
-    //                                           .dims = {2, 3},
-    //                                           .ndims = 2,
-    //                                           .data = {0, 1, 2, 3, 4, 5}};
-    // for (int dim = 0; dim < a->ndims; dim++) {
-    //   for (int idx = 0; idx < a->dims[dim]; idx++) {
-    //     printf("%d\t%d\n", dim, idx);
-    //   }
-    // }
-    puts("========================");
-    // call_vla(1, 2, 3, 4, 5, 6, 7);
-
-    // uint64_t nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    printf("result: %lu\n", prod(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+#define __VC_ARRAY_NEW(type)                                                   \
+  VC_ARRAY_TYPE(type) * vc_vec_new_##type(size_t ndims, size_t *dims) {        \
+    /* alloc array */                                                          \
+    VC_ARRAY_TYPE(type) *arr = malloc(sizeof(VC_ARRAY_TYPE(type)));            \
+                                                                               \
+    /* compute array size (product of dims) */                                 \
+    size_t len = dims[0];                                                      \
+    for (size_t i = 1; i < ndims; i++) {                                       \
+      if (len != 0) { /* ensures 0-len dims don't mess this up */              \
+        len *= dims[i];                                                        \
+      }                                                                        \
+    }                                                                          \
+    arr->len = len;                                                            \
+    arr->ndims = ndims;                                                        \
+    arr->itemsize = sizeof(type);                                              \
+    for (size_t i = 0; i < ndims; i++) {                                       \
+      arr->dims[i] = dims[i];                                                  \
+    }                                                                          \
+    arr->data = NULL;                                                          \
+    return arr;                                                                \
   }
+
+#define __VC_ARRAY_PRINT(type)                                                 \
+  void vc_vec_print_##type(VC_ARRAY_TYPE(type) * arr) {                        \
+    printf("array <%s> <%p>\n", #type, (void *)&arr);                          \
+    puts("{");                                                                 \
+    printf("  len: %lu\n", arr->len);                                          \
+    printf("  itemsize: %lu\n", arr->itemsize);                                \
+    printf("  ndims: %lu\n", arr->ndims);                                      \
+    printf("  dims: {\n");                                                     \
+    for (size_t i = 0; i < arr->ndims; i++) {                                  \
+      printf("    %lu: %lu\n", i, arr->dims[i]);                               \
+    }                                                                          \
+    printf("  }\n");                                                           \
+    printf("  data: {");                                                       \
+    if (arr->data) {                                                           \
+      for (size_t i = 0; i < arr->len; i++) {                                  \
+        printf(__vc_type_to_format_str(#type), &(arr->data)[i]);               \
+      }                                                                        \
+    }                                                                          \
+    printf(" }\n");                                                            \
+    printf("}\n");                                                             \
+  }
+
+typedef struct vc_array_idx_t {
+  size_t loc[MAX_ARRAY_DIM];
+  size_t current_dim;
+  size_t current_item;
+} vc_array_idx_t;
+
+//#define __VC_ARRAY_VIEW()
+
+//#define __VC_ARRAY_INIT_ITER(type)
+
+#define __VC_ARRAY_ITER(type)                                                  \
+  type vc_vec_iter##type(VC_ARRAY_TYPE(type) * arr, vc_array_idx_t * idx) {    \
+    size_t cd = idx->current_dim
+
+
+#define __VC_ARRAY_GEN_METHODS(type)                                           \
+  __VC_ARRAY_TYPEDEF(type)                                                     \
+  __VC_ARRAY_NEW(type)                                                         \
+  __VC_ARRAY_PRINT(type)
+
+__VC_ARRAY_GEN_METHODS(uint64_t)
+__VC_ARRAY_GEN_METHODS(float)
+__VC_ARRAY_GEN_METHODS(double)
+
+// static inline get_dims(dimensions, ...) {
+//
+// }
+
+int main() {
+  size_t ndims = 3;
+  size_t dims[] = {2, 3, 3};
+  vc_vec_double_t *arr = vc_vec_new_double(ndims, dims);
+  double *data = calloc(arr->len, arr->itemsize);
+  arr->data = &data;
+  vc_vec_print_double(arr);
+}
